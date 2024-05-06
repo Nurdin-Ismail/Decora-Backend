6d8d75163b91a1025ce50402faafc50262180f70
Nurdin Ismail <shiilnurdin@gmail.com>
2024-05-06 12:43:35 +0300

    changed outputs for get and patch request in userbyid in app.py

--- server/app.py
+++ server/app.py
@@ -182,6 +182,7 @@ class UserById(Resource):
                 "password": user.password,
                 "created_at" : user.created_at,
                 "updated_at" : user.updated_at,
+                'contacts' : user.contacts,
                 "cart" : cart
                 
             }
